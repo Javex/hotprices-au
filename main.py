7b9e216639a5fe76396b773ad3b7ffee36e13c4b
javex <florian.ruechel@inexplicity.de>
2023-09-27 18:37:03 +0930

    Add option for debug logging

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from hotprices_au import woolies, coles
 import argparse
+import logging
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--quick', action='store_true', default=False)
+    parser.add_argument('--debug', action='store_true', default=False)
     parser.add_argument('store', choices=['woolies', 'coles'])
     args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
     match args.store:
         case 'woolies':
             woolies.main(args.quick)
