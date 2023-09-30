cc425770a21a665e7f09d24be3fd82879cf20740
javex <florian.ruechel@inexplicity.de>
2023-09-30 12:20:53 +0930

    Add tests for analysis

---
+++ tests/test_analysis.py
@@ -0,0 +1,54 @@
+from hotprices_au import analysis
+
+def get_item(price=1, id='1'):
+    item = {
+        'store': 'foo',
+        'id': id,
+        'price': price,
+        'priceHistory': [{
+            'price': price,
+        }],
+    }
+    return item
+
+def test_merge_price_history():
+    # Price changed
+    old_items = [get_item()]
+    new_items = [get_item(price=2)]
+    items = analysis.merge_price_history(old_items, new_items)
+    assert len(items) == 1
+    item = items[0]
+    assert item['price'] == 2
+    assert len(item['priceHistory']) == 2
+    assert item['priceHistory'][0]['price'] == 2
+    assert item['priceHistory'][1]['price'] == 1
+
+    # Price didn't change
+    old_items = [get_item()]
+    new_items = [get_item()]
+    items = analysis.merge_price_history(old_items, new_items)
+    assert len(items) == 1
+    item = items[0]
+    assert item['price'] == 1
+    assert len(item['priceHistory']) == 1
+    assert item['priceHistory'][0]['price'] == 1
+
+    # No past history
+    new_items = [get_item(price=2)]
+    items = analysis.merge_price_history(None, new_items)
+    assert len(items) == 1
+    item = items[0]
+    assert item['price'] == 2
+    assert len(item['priceHistory']) == 1
+    assert item['priceHistory'][0]['price'] == 2
+
+    # Multiple products
+    old_items = [get_item(price=3), get_item(id='2')]
+    new_items = [get_item(price=2), get_item(id='2')]
+    items = analysis.merge_price_history(old_items, new_items)
+    assert len(items) == 2
+    item = items[0]
+    assert item['price'] == 2
+    assert len(item['priceHistory']) == 2
+    assert item['priceHistory'][0]['price'] == 2
+    assert item['priceHistory'][1]['price'] == 3
